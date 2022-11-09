d223e6c1408e0b1e08e54ccabd1cf3bf66806c63
Lucas da Silva Bezerra <lucasdsilvab@gmail.com>
2022-11-09 19:57:23 -0300

    estudos node

---
+++ src/controllers/homeController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+
+import { Op } from 'sequelize'
+import { Product } from '../models/Product';
+import { User } from '../models/User'
+
+export const home = async (req: Request, res: Response) => {
+
+
+    const [usuario, created] = await User.findOrCreate({
+        where: { name: 'Bonieky' },
+        defaults: {
+            age: 80
+        }
+    })
+
+    if (created) {
+        console.log('Usuário criado com sucesso!')
+    } else {
+        console.log('Achamos o usuários')
+    }
+    console.log('NOME: ', usuario.name)
+
+
+    let users = await User.findAll()
+
+    let age: number = 90;
+    let showOld: boolean = false;
+
+    if (age > 50) {
+        showOld = true;
+    }
+
+    let list = Product.getAll();
+    let expensiveList = Product.getFromPriceAfter(12);
+
+    res.render('pages/home', {
+        name: 'Bonieky',
+        lastName: 'Lacerda',
+        showOld,
+        products: list,
+        expensives: expensiveList,
+        frasesDoDia: [],
+        users
+    });
+
+};
